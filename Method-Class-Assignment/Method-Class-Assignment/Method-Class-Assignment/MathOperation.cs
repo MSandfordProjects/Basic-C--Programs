5b3ab116214501ee4ac51b817a89d68f2e47ee30
AcademySchoolProject <148668570+MSandfordProjects@users.noreply.github.com>
2024-02-26 22:25:18 -0700

    Blackjack and Method
    Method Assignment, some blackjack

---
+++ Method-Class-Assignment/Method-Class-Assignment/Method-Class-Assignment/MathOperation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class MathOperation
+    {
+        //Create a class. In that class, create a void method that takes two integers as parameters.
+        public void MathMethod(int number1, int number2)
+        {
+            //Have the method do a math operation on the first integer  
+            int OperationNum1 = number1 * 8;
+
+            //and display the second integer to the screen.
+            Console.WriteLine("The Second Number is " + number2 + ". Press Enter");
+            Console.ReadLine();
+
+            //display the math operation on the screen.
+            Console.WriteLine("The First number" + number1 + " multiplied by 8 is " + OperationNum1);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method-Class-Assignment/Method-Class-Assignment/Method-Class-Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class.
+            MathOperation Operation = new MathOperation();
+            //write on the screen asking to pick a number
+            Console.WriteLine("Please pick a number: ");
+            //input that as number1
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            //write on the screen asking to pick another number
+            Console.WriteLine("Please pick another number: ");
+            //input that as number2
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            //Call the method in the class, passing in two numbers. 
+            Operation.MathMethod(number1, number2);
+
+            //could also do Operation.MathMethod(12, 10); with preset numbers instead of asking for numbers.
+
+            //Call the method in the class, specifying the parameters by name.
+            Operation.MathMethod(number1: number1, number2: number2);
+
+        }
+    }
+}
