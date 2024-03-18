ed9e8adaaf27f002fb7cffe116d7212bc50b9631
AcademySchoolProject <148668570+MSandfordProjects@users.noreply.github.com>
2024-03-18 06:19:17 -0600

    student Database

---
+++ StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var addStudent = new Student()
+            {
+                Id = 3351,
+                FirstName = "Jesse",
+                LastName = "Black"
+            };
+
+            using (var dbContext = new StudentDbContext())
+            {
+                dbContext.Students.Add(addStudent);
+                dbContext.SaveChanges();
+            }
+
+            Console.WriteLine("Added Student");
+            Console.ReadLine();
+        }
+    }
+}
